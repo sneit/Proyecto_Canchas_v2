69fe7573a666b20196c89397b94a90a7c2bade47
Ricardo Ramos <suneitto_27@hotmail.com>
2017-08-26 17:38:50 -0500

    nuevo

---
+++ app/controllers/pagredsocial_controller.rb
@@ -0,0 +1,20 @@
+class PagredsocialController < ApplicationController
+  layout 'application'
+  def fotoscanchita
+  end
+  
+  def fotoscanchita2
+  end
+  
+  def caracteristica
+  end
+
+  def integrante
+  end
+
+  def miembro
+  end
+
+  def contactenos
+  end
+end
